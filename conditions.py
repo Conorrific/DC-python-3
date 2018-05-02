3c785c00ff5a78cd40a29a7b3298cadbce393004
conorrific <conorbellchambers@gmail.com>
2018-05-02 11:03:18 -0500

    Conditional statements

---
+++ conditions.py
@@ -0,0 +1,13 @@
+age = int(input("Enter age"))
+is_clean_record = True
+if 2 < 21:
+    print("Can't apply for license!")
+elif age > 100 or is_clean_record == True:
+    print("Too old to drive!")
+elif age > 18 age is_clean_record == True:
+    print("Sure you can apply!")
+else:
+    print("do something!")    
+#and means both have to be true. 
+# or means that one or the other has to be true
+#the last line is an else statement meaning if everything else fails.

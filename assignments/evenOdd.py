f10a906038761e0993ea87e118a044d42b63cda4
conorrific <conorbellchambers@gmail.com>
2018-05-02 12:07:28 -0500

    EvenOdd I hope this works too

---
+++ assignments/evenOdd.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number and I'll tell you if its even or odd: "))
+def game():
+    if num % 2 == 0:
+        print(f"The number {num} is even")
+    else:
+        print(f"The number {num} is odd")
+game()        

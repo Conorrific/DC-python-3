c7402e9e8d43b5186860f9780297be3527e16f55
conorrific <conorbellchambers@gmail.com>
2018-05-02 14:55:48 -0500

    updated to accept c or f

--- assignments/temp_converter.py
+++ assignments/temp_converter.py
@@ -1,3 +1,10 @@
-outside = int(input("Enter a temperature in degrees celcius: "))
-fahrenheit = str((outside * 1.8) + 32)
-print("Your temperature in Fahrenheit is " + fahrenheit + " degrees!")
+outside = float(input("Enter a temperature, and I'll convert it: "))
+temptype = input("is this celcius or fahrenheit? answer with c or f ")
+if temptype == "c":
+    newtemp = str(round((outside * 1.8) + 32, 2))
+    print("Your temperature in Fahrenheit is " + newtemp + " degrees!", 2)
+elif temptype == "f":
+    newtemp = str(round((outside - 32 ) / 1.8, 2))
+    print("Your temperature in Celcius is " + newtemp + " degrees!", 2)
+else:
+    print("Invalid answer, sorry!")
